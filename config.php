cc672e1da32c8594e70d6e36c5b10a50ca178404
Ajay Kumar Choudhary <ajay@froiden.com>
2016-06-02 16:53:44 +0530

    Database connection changed to PDO

--- config.php
+++ config.php
@@ -2,21 +2,29 @@
 	ini_set('max_execution_time', 0);
 	ini_set('memory_limit', '-1');
 	
-#------------------Changes to be done here-----------------------------
-	
-	$host='localhost'; #hostname
-	$user='root';          #user
-	$password='';      #password
-	$database='gabned';      #database name
+#***********************************Changes to be done here**************************************************
+	$GLOBALS["HOST"]     = "localhost";
+	$GLOBALS["USERNAME"] = "root";
+	$GLOBALS["PASSWORD"] = "";
+	$GLOBALS["DATABASE"] = "geonames";
+
 	#path of the download folder which is in the geonames folder..Dont forget to end with /
-	$path='/Install Directory/xampp/htdocs/geonames/download/';
+	$path='/Users/Ajay/htdocs/geonames/download/';
 	
 #*****************************************************************************	
-	
-	#Database base connection string
-	$con=mysql_connect($host,$user,$password);
-	mysql_select_db($database,$con);
-	
-	#opening connection for the insertion og utf8 characters
-	mysql_query("SET NAMES utf8");
+
+	$db = connectDatabase();
+
+	function connectDatabase() {
+		try{
+			$db = new PDO("mysql:dbname={$GLOBALS["DATABASE"]};host={$GLOBALS["HOST"]}", $GLOBALS["USERNAME"], $GLOBALS["PASSWORD"]);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$db->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
+			return $db;
+		}catch(PDOException $ex)
+		{
+			die(json_encode(array('outcome' => false, 'message' => 'Database connection failed')));
+		}
+	}
+
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	
-	include 'config.php';	
+	include 'config.php';
+
 #------------------CODE FOR DOWNLOADING THE FILES --------------
 	$download=array(
 	'admin1CodesASCII.txt',
@@ -9,18 +10,24 @@
 	'iso-languagecodes.txt',
 	'alternateNames.zip',
 	'allCountries.zip',
-	'countryInfo.txt');
-	
+	'countryInfo.txt'
+	);
+
 	foreach($download as $file){
+		if (!file_exists('download')) {
+			$dirMode = 0777;
+			$directory = "download";
+			chmod($directory, $dirMode);
+			mkdir($directory, $dirMode, true);
+		}
 		$destination = "download/$file";
-		 
+
 		if (file_exists($destination)) {
 		   echo "The file <b>$file</b> already exists in the download Folder<br> ";
         }else{
-			
+
 	   	 	$ch = curl_init();
 			$source = "http://download.geonames.org/export/dump/$file ";
-			
 			curl_setopt($ch, CURLOPT_URL, $source);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			$data = curl_exec ($ch);
@@ -29,7 +36,7 @@
 			fputs($file, $data);
 			fclose($file);
 		}
-		 
+
 	}
 	
 #**********************END OF CODE FOR DOWNLOADING***************************
@@ -63,54 +70,55 @@
 	
 #---------------------CODE FOR TABLES CREATION AND DATABASE INSERTION----------------------
 	#Database base connection string
-	$con=mysql_connect($host,$user,$password);
-	mysql_select_db($database,$con);
-	
-	#opening connection for the insertion og utf8 characters
-	mysql_query("SET NAMES utf8");
-	
-	#Code for truncationg the already inserted database the tables
-	$query="SHOW TABLES";
-	$result =mysql_query($query);
-	while($data = mysql_fetch_array($result)){
-				mysql_query("TRUNCATE ".$data["Tables_in_$database"]);
-	}
-	
+
+	#Code for truncating the already inserted database the tables
+	$query  =   "SHOW TABLES";
+	$statement  = $db->prepare($query);
+	$statement->execute();
+
+	$data = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+	foreach($data as $table){
+		$query ="TRUNCATE ".$table;
+		$statement1  = $db->prepare($query);
+		$statement1->execute();
+	};
+
 	#Code for the creation of tables
-	$query=file_get_contents('tables.sql');
-	$statement=mysql_query($query);
+	$query  =   file_get_contents('tables.sql');
+
+	$statement  = $db->prepare($query);
+	$statement->execute();
+
 
 	#Array of filename and the table name 
-	$tablenames=array(
+	$tablenames =   array(
 		#txt name,table name
 		array('allCountries.txt','geo_name'),
 		array('alternateNames.txt','geo_alternateName'),
 		array('admin1CodesASCII.txt','geo_admin1'),
 		array('timeZones.txt','geo_timeZones'),
-		
 		array('featureCodes_en.txt','geo_featureCodes'),
 		array('iso-languagecodes.txt','geo_languagecodes'),
-		
 		array('countryInfo.txt','geo_country')
 	
 	);
-	
-foreach($tablenames as $file){
-	
-		$query="load data infile '{$path}{$file['0']}' IGNORE INTO TABLE {$file['1']} CHARACTER SET UTF8;";	
-	    $statement=mysql_query($query);		
-}	
 
-	#Delete empty rows from countryIndo
+	foreach($tablenames as $file){
+
+			$query  =   "load data infile '{$path}{$file['0']}' IGNORE INTO TABLE {$file['1']} CHARACTER SET UTF8;";
+			$statement  = $db->prepare($query);
+			$statement->execute();
+	}
+
+	#Delete empty rows from countryInfo
 	$query="
 	DELETE FROM geo_country where iso_alpha2 LIKE '#%';
 	DELETE FROM geo_languagecodes where iso_639_3='ISO';
 	DELETE FROM geo_timezones where countrycode='CountryCode';
 	";
-	$statement=mysql_query($query);
-	if ($statement) {
-		echo 'DElETED';
-	} 
+	$statement  = $db->prepare($query);
+	$statement->execute();
 #******************END OF CODE FOR TABLES CREATION AND DATABASE INSERTION***************************
 	
 ?>
